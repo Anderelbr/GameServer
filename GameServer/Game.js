aded74488a9d9ee0a5b8a43fe50c117096a074f6
Anderelbr <anderelbr20@gmail.com>
2016-11-02 11:11:31 -0300

    Add files via upload

--- GameServer/Game.js
+++ GameServer/Game.js
@@ -1,6 +1,11 @@
 var mongodb = require('mongodb').MongoClient;
 var config = require('./config.js');
 
+var shortid = require('shortid');
+
+var players = [];
+var buffer = [];
+
 function ConnectDB (callback){
 	mongodb.connect(config.dburl, function (err, db){
 		if(err)
@@ -10,23 +15,123 @@ function ConnectDB (callback){
 	});
 };
 
-function EnterWorld (socket){
-
-	this.LoadPlayer = function (data){
-
-		var channel = data.channel;
-		var charid = {charid:data.charid};
-
+function Server (socket){    
+    var player = {
+        playerid: "",
+                        
+        name: "",
+        channel: "",
+                        
+        money: 0,
+        level: 0,
+                        
+        location: "",
+        pos: "",
+                        
+        accid: "",
+        charid: "",
+                        
+        objid: "",
+        
+        moveSpeed: 0,
+        rotSpeed: 0,
+          
+        lastposition: {
+            x: 0,
+            y: 0,
+            z: 0
+        },
+                        
+        lastmovetime: 0
+    };
+    
+	this.LoadPlayer = function (data){ 
 		ConnectDB(function(db){
-			db.collection("characters").find(charid).toArray(function(err, result){
-				if(result.length > 0){
-					socket.sockets.in(channel).emit('WorldEnterRes', {loadedplayer:result[0], opcode:"0"});
-				}else{
-					socket.sockets.in(channel).emit('WorldEnterRes', {opcode:"1"});
+			db.collection("characters").find({charid:data.charid}).toArray(function(err, result){
+				if(result.length > 0){  
+                    player = {
+                    playerid: shortid.generate(),
+
+                    name: result[0].name,
+                    channel: data.channel,
+
+                    money: result[0].money,
+                    level: result[0].level,
+
+                    location: result[0].location,
+                    pos: result[0].pos,
+
+                    accid: result[0].accid,
+                    charid: result[0].charid,
+
+                    objid: result[0].objid,
+                        
+                    moveSpeed: result[0].moveSpeed,
+                    rotSpeed: result[0].rotSpeed,
+
+                    lastposition: {
+                        x: 0,
+                        y: 0,
+                        z: 0
+                    },
+
+                    lastmovetime: 0
+                    };  
+                    
+                    for(var i = 0; i < players.length; i++){  
+                        //This part load all players that now are connected
+                        socket.emit('WorldEnterRes', {loadedplayer:players[i], opcode:"1"}); 
+                    };
+                    
+                    //This part add my player to list of characters and send a message to the client but instantiate my player
+                    players.push(player);
+                    socket.emit('WorldEnterRes', {loadedplayer:player, opcode:"0"});
+                    
+                    //This part send to all players connected that i'm now are connected and instantiate my player
+                    socket.broadcast.to(player.channel).emit('WorldEnterRes', {loadedplayer:player, opcode:"1"});
+                }else{
+					socket.emit('WorldEnterRes', {message: "Fail to create character", opcode:"3"});
 				};
 			});
 		});
 	};
-};
+    
+    this.Move = function (data){   
+    
+    var clientdate = new Date(Date.parse(data.time));    
+        
+    var playerindex = players.indexOf(player);       
+    var moveData = {posx: data.pos.x, posy: data.pos.y, posz: data.pos.z, moveSpeed: players[playerindex].moveSpeed, rotSpeed: players[playerindex].rotSpeed, state: data.state, time:Date.now(), playerid: players[playerindex].playerid};  
+         
+    if(buffer.length >= 10){
+        for (var i = buffer.length - 1; i > 0; i--){
+            //EJ: Slot 20 = Slot 19
+            buffer[i] = buffer[i - 1];
+        };
+    }else{
+        buffer.push(moveData);
+    };
+        
+    buffer[0] = moveData;    
+        
+    if(buffer[0].time > clientdate.getTime()){
+        for(var i = 0; i < buffer.length; i++){
+          if(buffer[i].time <= clientdate.getTime() || i === buffer.length - 1){
+              socket.broadcast.to(players[playerindex].channel).emit('MoveRes', buffer[i]);  
+          };
+        };
+    };    
+    }
+    
+    this.DisconnectedPlayer = function (data){   
 
-module.exports.EnterWorld = EnterWorld;
+    var playerindex = players.indexOf(player);
+        
+    socket.broadcast.to(players[playerindex].channel).emit('DisconnectRes', {playerid: players[playerindex].playerid});
+    console.log("Player Disconnected: " + players[playerindex].name);
+        
+    players.splice(playerindex, 1);
+    };
+}
+
+module.exports.Server = Server;
--- GameServer/GameServer.js
+++ GameServer/GameServer.js
@@ -1,58 +1,64 @@
 var room = require('./Room.js').room;
 var game = require('./Game.js');
 
+var shortid = require('shortid');
+
 var app = require('express')();
 var http = require('http').Server(app);
-var ioo = require('socket.io');
-var shortid = require('shortid');
+var io = require('socket.io');
 
 var port = process.env.PORT || 7300;
 
 var rooms = [];
 var maxrooms = 3;
 
+var serverunning = false;
+
+function StartEvents(server,socket){
+    socket.on ('GameEnterReq', function(){
+        if(rooms[0].cursize <  rooms[0].size){         
+            socket.room = rooms[0].name;
+            socket.join(socket.room);
+
+            rooms[0].cursize++;
+
+            console.log("Welcome, you are connected to " + rooms[0].name);
+            console.log("Current Size: " + rooms[0].cursize + "/" + rooms[0].size);
+
+            socket.emit('GameEnterRes', {channel: socket.room, opcode:"0"});
+        };
+    });
+    
+    socket.on('MoveReq', server.Move);
+    socket.on('WorldEnterReq', server.LoadPlayer);  
+    
+    socket.on('disconnect', server.DisconnectedPlayer);
+};
+
+function InitializeServer(){
+    if(!serverunning){
+        
+	io = io.listen(http, false);
+    
+	io.on('connection', function (socket){
+		var server = new game.Server(socket);
+		StartEvents(server,socket);
+	});
+
+	//TODO:Create Channels
+	for (var i = 0; i < maxrooms; i++){
+		var curoom = new room ('Channel ' + i, 'Available', shortid.generate(), 0, 20);
+		rooms.push(curoom);
+	};
+    
+    serverunning = true;
+	console.log("Server is running on " + port);
+    };
+};
 
 app.get('/', function (req, res){
 	res.send("<h1>Server is running on " + port + "</h1>");
 	InitializeServer();
 });
-
-function InitializeServer(){
-	var io = ioo.listen(http, false);
-
-	io.on('connection', function (socket){
-		var server = new game.EnterWorld(io);
-		StartEvents(server,socket);
-	});
-
-	//Create Channels
-	for (var i = 0; i < maxrooms; i++){
-		var curoom = new room ('Channel ' + i, 'Available', shortid.generate(), 0, 3);
-		rooms.push(curoom);
-	};
-
-	console.log("Server is running on " + port);
-};
-
-function StartEvents(server,socket){
-
-socket.on ('GameEnterReq', function(){
-			if(rooms[0].cursize <  rooms[0].size){
-
-			socket.room = rooms[0].name;
-			socket.join(socket.room);
-
-			//rooms[0].cursize++;
-
-			console.log("Welcome, you are connected to " + rooms[0].name);
-			console.log("Size: " + rooms[0].size);
-			console.log("Current Size: " + rooms[0].cursize);
-
-			socket.emit('GameEnterRes', {channel: socket.room, opcode:"0"});
-			};
-		});
-
-		socket.on('WorldEnterReq', server.LoadPlayer);
-};
 
 http.listen(port);
